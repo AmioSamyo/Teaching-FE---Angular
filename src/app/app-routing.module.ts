a9e60c16f9daac9f327896044676f6298004ad8b
Alessio <alessiomalfissi99@outlook.it>
2022-03-30 16:06:15 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PlannerComponent} from "./components/planner/planner.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PlannerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
